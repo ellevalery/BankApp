0a7e88e7816905b7eece6aa59e09db2667b9271f
Valeria Zalygina <lerazalygina@gmail.com>
2023-07-10 11:25:57 +0300

    BankApp last changes with JOptionPan

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.JOptionPane;
 import java.math.BigDecimal;
 import java.util.Random;
 import java.util.Scanner;
@@ -6,57 +7,66 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static User user = new User("name", "surname", "female", 12345566L, "EUR", new BigDecimal("22500.20"));
     private static final BankApp bankApp = new BankApp();
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         bankApp.getUsersOfBank().add(user);
 
-        System.out.println("Welcome to Our bank. Please choose the option:");
+        JOptionPane.showMessageDialog(null, "Welcome to Our bank. Please choose an option:");
         printAMenu();
 
         while (true) {
-            char choice = scanner.next().charAt(0);
+            String choice = JOptionPane.showInputDialog("Enter your choice:");
+            if (choice == null) {
+                // User clicked on cancel or closed the dialog
+                System.exit(0);
+            }
+
             switch (choice) {
-                case '1' -> registerNewUser();
-                case '2' -> displayUserBalance();
-                case '3' -> depositMoney();
-                case '4' -> withdrawMoney();
-                case 'x' -> {
-                    System.out.println("Thank you for using our service");
+                case "1" -> registerNewUser();
+                case "2" -> displayUserBalance();
+                case "3" -> depositMoney();
+                case "4" -> withdrawMoney();
+                case "5" -> {
+                    JOptionPane.showMessageDialog(null, "Thank you for using our service");
                     System.exit(0);
                 }
-                default -> System.out.println("Please choose one from a menu");
+                default -> JOptionPane.showMessageDialog(null, "Please choose one from the menu");
             }
         }
     }
+
     private static void printAMenu() {
-        System.out.println("""
-        Bank menu:\s
-        1 - to register as a new user\s
-        2 - to see your balance\s
-        3 - to deposit money
-        4 - to withdraw money
-        x - to end a program\s""");
+        JOptionPane.showMessageDialog(null, """
+                Bank menu:\s
+                1 - to register as a new user\s
+                2 - to see your balance\s
+                3 - to deposit money
+                4 - to withdraw money
+                5 - to end the program\s""");
     }
+
     private static void registerNewUser() {
-        System.out.println("Enter your name");
-        scanner.nextLine();
-        String name = scanner.nextLine();
-        System.out.println("Enter your surname");
-        String surname = scanner.nextLine();
-        System.out.println("Enter your gender");
-        String gender = scanner.nextLine();
-        System.out.println("Select your account currency: 1-EUR, 2-USD");
-        String currency = "EUR";
+        String name = JOptionPane.showInputDialog("Enter your name");
+        String surname = JOptionPane.showInputDialog("Enter your surname");
+        String gender = JOptionPane.showInputDialog("Enter your gender");
 
-        char option = scanner.next().charAt(0);
-        while (!(option == '1' || option == '2')) {
-            System.out.println("Choose a number from 1 to 2!");
+        String[] options = {"1 - EUR", "2 - USD"};
+        int currencyChoice = JOptionPane.showOptionDialog(
+                null,
+                "Select your account currency:",
+                "Currency Selection",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
 
-            option = scanner.next().charAt(0);
-            switch (option) {
-                case '1' -> currency = "EUR";
-                case '2' -> currency = "USD";
-            }
+        String currency;
+        switch (currencyChoice) {
+            case 0 -> currency = "EUR";
+            case 1 -> currency = "USD";
+            default -> currency = "EUR"; // Default currency is EUR
         }
 
         long min = 1000000000L;
@@ -65,52 +75,66 @@ public class Main {
         long accountNumber = random.nextLong() % (max - min) + max;
         User registeredUser = new User(name, surname, gender, accountNumber, currency, BigDecimal.ZERO);
         user = bankApp.createNewUser(registeredUser);
-        System.out.println(user);
+        JOptionPane.showMessageDialog(null, user.toString());
         printAMenu();
     }
 
     private static void displayUserBalance() {
         if (user == null) {
-            System.out.println("There is no registered user in our system");
+            JOptionPane.showMessageDialog(null, "There is no registered user in our system");
         } else {
             bankApp.displayUserBalance(user);
         }
         printAMenu();
     }
 
-    private static void withdrawMoney(){
+    private static void withdrawMoney() {
         if (user == null) {
-            System.out.println("There is no registered user in our system");
+            JOptionPane.showMessageDialog(null, "There is no registered user in our system");
         } else {
             BigDecimal number;
             do {
-                System.out.println("Please enter amount you want to withdraw: ");
-                while (!scanner.hasNextBigDecimal()) {
-                    System.out.println("Please enter a positive number!");
-                    scanner.next();
+                String input = JOptionPane.showInputDialog("Please enter the amount you want to withdraw:");
+                if (input == null) {
+                    // User clicked on cancel or closed the dialog
+                    return;
                 }
-                number = scanner.nextBigDecimal();
-            } while (number.compareTo(BigDecimal.ZERO) != 1);
+                try {
+                    number = new BigDecimal(input);
+                } catch (NumberFormatException e) {
+                    number = BigDecimal.ZERO;
+                }
+                if (number.compareTo(BigDecimal.ZERO) <= 0) {
+                    JOptionPane.showMessageDialog(null, "Please enter a positive number!");
+                }
+            } while (number.compareTo(BigDecimal.ZERO) <= 0);
             bankApp.WithdrawMoney(user, number);
             printAMenu();
         }
     }
-    private static void depositMoney(){
+
+    private static void depositMoney() {
         if (user == null) {
-            System.out.println("There is no registered user in our system");
+            JOptionPane.showMessageDialog(null, "There is no registered user in our system");
         } else {
             BigDecimal number;
             do {
-                System.out.println("Please enter amount you want to deposit: ");
-                while (!scanner.hasNextBigDecimal()) {
-                    System.out.println("Please enter a positive number!");
-                    scanner.next();
+                String input = JOptionPane.showInputDialog("Please enter the amount you want to deposit:");
+                if (input == null) {
+                    // User clicked on cancel or closed the dialog
+                    return;
                 }
-                number = scanner.nextBigDecimal();
-            } while (number.compareTo(BigDecimal.ZERO) != 1);
+                try {
+                    number = new BigDecimal(input);
+                } catch (NumberFormatException e) {
+                    number = BigDecimal.ZERO;
+                }
+                if (number.compareTo(BigDecimal.ZERO) <= 0) {
+                    JOptionPane.showMessageDialog(null, "Please enter a positive number!");
+                }
+            } while (number.compareTo(BigDecimal.ZERO) <= 0);
             bankApp.DepositMoney(user, number);
             printAMenu();
         }
     }
 }
-
