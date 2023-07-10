b1fb9bc8108536e0e1bafff64c44c11758acef09
Valeria Zalygina <lerazalygina@gmail.com>
2023-07-10 11:17:55 +0300

    BankApp last changes

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,6 +3,7 @@ import java.math.BigDecimal;
 public class User {
     private final String name;
     private final String surname;
+    private Integer age;
     private final String gender;
     private final long accountNumber;
     private final String accountCurrency;
@@ -11,10 +12,24 @@ public class User {
     public User(String name, String surname, String gender, long accountNumber, String accountCurrency, BigDecimal balance) {
         this.name = name;
         this.surname = surname;
+        this.age = age;
         this.gender = gender;
         this.accountNumber = accountNumber;
         this.accountCurrency = accountCurrency;
         this.balance = balance;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User " +
+                "name: '" + name + '\'' +
+                ", surname: '" + surname + '\'' +
+                ", age=" + age +
+                ", gender: '" + gender + '\'' +
+                ", accountNumber: " + accountNumber +
+                ", mainCurrency: " + accountCurrency + '\'' +
+                ", balance: " + balance;
     }
 
     public BigDecimal getBalance() {
@@ -24,18 +39,9 @@ public class User {
     public void setBalance(BigDecimal balance) {
         this.balance = balance;
     }
+
     public String getAccountCurrency() {
         return accountCurrency;
-    }
-    @Override
-    public String toString() {
-        return "User " +
-                "name: '" + name + '\'' +
-                ", surname: '" + surname + '\'' +
-                ", gender: '" + gender + '\'' +
-                ", accountNumber: " + accountNumber +
-                ", mainCurrency: " + accountCurrency + '\'' +
-                ", balance: " + balance;
     }
 }
 
